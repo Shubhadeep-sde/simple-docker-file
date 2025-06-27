a786334b25f658b6e1ea98ba0cd2c2fd37c6b8b4
Shubhadeep <shubhadeep.pal.sde@gmail.com>
2025-06-27 23:22:04 +0530

    first commit

---
+++ src/main/java/com/personal/docker_demo/DockerDemoApplication.java
@@ -0,0 +1,14 @@
+package com.personal.docker_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+		System.out.println("My springboot application is running");
+	}
+
+}
