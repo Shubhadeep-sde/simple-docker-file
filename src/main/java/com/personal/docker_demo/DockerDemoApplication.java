b63d2990a0e6da8b26ad5b4ab117f9f0397ec386
Shubhadeep <shubhadeep.pal.sde@gmail.com>
2025-06-28 12:29:55 +0530

    Updated messages

--- src/main/java/com/personal/docker_demo/DockerDemoApplication.java
+++ src/main/java/com/personal/docker_demo/DockerDemoApplication.java
@@ -8,7 +8,7 @@ public class DockerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerDemoApplication.class, args);
-		System.out.println("My springboot application is running");
+		System.out.println("My springboot application is running in docker");
 	}
 
 }
